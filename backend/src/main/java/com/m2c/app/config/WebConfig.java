dd390e162af5277c195a2733397e1d539126f33c
exploring-curiosity <sr7431@nyu.edu>
2026-02-04 02:25:00 -0500

    Use Preflight cors setting instead of CORS filter

--- backend/src/main/java/com/m2c/app/config/WebConfig.java
+++ backend/src/main/java/com/m2c/app/config/WebConfig.java
@@ -4,8 +4,8 @@ import com.m2c.app.web.session.SessionUserResolver;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-import org.springframework.web.filter.CorsFilter;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -23,7 +23,7 @@ public class WebConfig implements WebMvcConfigurer {
     }
 
     @Bean
-    public CorsFilter corsFilter() {
+    public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
         config.setAllowCredentials(true);
         String[] allowedOrigins = appProperties.getCors().getAllowedOrigins();
@@ -35,7 +35,7 @@ public class WebConfig implements WebMvcConfigurer {
 
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", config);
-        return new CorsFilter(source);
+        return source;
     }
 
     @Override
